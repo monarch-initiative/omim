fdc3abf3a38345ddd93304c1048518608142b822
joeflack4 <joeflack4@gmail.com>
2023-06-19 16:13:59 -0400

    ODK-based GitHub Action - Update: GH action workflow: (i) now uses ODK container, (ii) Had to change 'echo -en' to plain 'echo' due to some new incompatibility with new ODK environment, (iii) removed some comments; streamlined. - Update: Minor updates in a few files; code comments, codestyle, verbosity

--- omim2obo/config.py
+++ omim2obo/config.py
@@ -1,9 +1,11 @@
+"""Configuration"""
 from pathlib import Path
 import yaml
 from dotenv import dotenv_values
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR / 'data'
+ENV_PATH = ROOT_DIR / '.env'
 
 with open(DATA_DIR / 'dipper/GLOBAL_TERMS.yaml') as file:
     GLOBAL_TERMS = yaml.safe_load(file)
@@ -11,4 +13,4 @@ with open(DATA_DIR / 'dipper/GLOBAL_TERMS.yaml') as file:
 with open(DATA_DIR / 'dipper/curie_map.yaml') as file:
     CURIE_MAP = yaml.safe_load(file)
 
-config = dotenv_values(ROOT_DIR / '.env')
+CONFIG = dotenv_values(ENV_PATH)
