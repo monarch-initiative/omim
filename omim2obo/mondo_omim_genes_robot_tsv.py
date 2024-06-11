cb615e8f94bb40b2a7cf274eafde0904e9845244
Joe Flack <joeflack4@gmail.com>
2024-06-11 18:21:35 -0400

    HGNC ROBOT template - Update: Refactor method to do this from ShellScript / awk to Python / pandas. - Update: Now sorts columns
    General
    - Update: .gitignore: Simplified ignores for files at root.
    - Add: Utility function to handle < > around URIs

---
+++ omim2obo/mondo_omim_genes_robot_tsv.py
@@ -0,0 +1,64 @@
+"""Create: ROBOT template of Mondo and OMIM gene relations: relational information for gene and disease classes"""
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Dict, Union
+
+import pandas as pd
+
+from omim2obo.utils.utils import remove_angle_brackets
+
+
+ROBOT_SUBHEADER = {
+    'mondo_id': 'ID',
+    'hgnc_id': "SC 'has material basis in germline mutation in' some %",
+    'omim_disease_xref': '>A oboInOwl:source',
+    'source_code': '>A oboInOwl:source',
+    'omim_gene': '',
+}
+
+
+def mondo_omim_genes_robot_tsv(inpath: Union[Path, str], outpath: Union[Path, str]) -> pd.DataFrame:
+    """Create: ROBOT template of Mondo and OMIM gene relations"""
+    df = pd.read_csv(inpath, sep='\t')
+
+    # Remove the first character, a question mark (?), from each field in the header; an artefact of the SPARQL query.
+    df.rename(columns={col: col[1:] for col in df.columns if col.startswith('?')}, inplace=True)
+
+    # Add source_code column
+    df['source_code'] = 'MONDO:OMIM'
+
+    # Remove < and > characters from specified columns
+    uri_cols = ['mondo_id', 'hgnc_id', 'omim_gene']
+    for col in uri_cols:
+        df[col] = remove_angle_brackets(list(df[col]))
+
+    # Insert ROBOT subheader
+    df = pd.concat([pd.DataFrame([ROBOT_SUBHEADER]), df])
+
+    # Format col order
+    df = df[['mondo_id', 'hgnc_id', 'omim_disease_xref', 'source_code', 'omim_gene']]
+
+    # Sort
+    df = df.sort_values(by=['mondo_id', 'hgnc_id', 'omim_gene', 'omim_disease_xref'])
+
+    df.to_csv(outpath, sep='\t', index=False)
+    return pd.DataFrame()
+
+
+def cli():
+    """Command line interface."""
+    parser = ArgumentParser(
+        prog='mondo-genes-robot-tsv',
+        description='Create a ROBOT template TSV of relational information for gene and disease classes')
+    parser.add_argument(
+        '-i', '--inpath', required=True,
+        help='Path to file with such relational information, but not yet formatted as a ROBOT template.')
+    parser.add_argument(
+        '-o', '--outpath', required=True,
+        help='Path to save output.')
+    d: Dict = vars(parser.parse_args())
+    mondo_omim_genes_robot_tsv(**d)
+
+
+if __name__ == '__main__':
+    cli()
