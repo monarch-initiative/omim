afd7b751c3411a69aa85c0dd6cdae469366041b4
Joe Flack <joeflack4@gmail.com>
2024-09-22 19:39:34 -0400

    Removed duplicate oboInOwl namespace declaration

--- omim2obo/namespaces.py
+++ omim2obo/namespaces.py
@@ -53,7 +53,6 @@ OBA = Namespace('http://purl.obolibrary.org/obo/OBA_')                        # 
 OBAN = Namespace('http://purl.org/oban/')                                     # Open Biomedical Annotation Model
 OBI = Namespace('http://purl.obolibrary.org/obo/OBI_')                        # Ontology of Biomedical Investigations
 OBO = Namespace('http://purl.obolibrary.org/obo/')                            # Biological Ontology namespace (this is not itself an ontology)
-OBOINOWL = Namespace('http://www.geneontology.org/formats/oboInOwl#')         # obo-specific annotation properties, like synonym types
 # 'OMIA' was http://omia.angis.org.au/ IS https://omia.org/         # (see about helping to update original data)
 OMIA = Namespace('https://omia.org/OMIA')                                     # Online Mendelian Inheritance in Animals (disease/species)
 # LIDIA seems retired. so these are not resovable                   # Also: http://www.vetsci.usyd.edu.au/lida/
