bfaaa9a9eb1fdf0648b875fc30d82eb159a5422a
Joe Flack <joeflack4@gmail.com>
2025-04-21 17:21:52 -0400

    Disease-gene protections updates - Update: Refactored recent code for simplification and efficiency, reducing a transformation to only be done one time. - Update: Varible names, code comments, and type declarations to match recent code changes.

--- omim2obo/main.py
+++ omim2obo/main.py
@@ -375,7 +375,8 @@ def omim2obo(use_cache: bool = False):
 
     # - Add relations (subclass restrictions)
     exclusions_p_mim_orcid_map: Dict[str, Optional[URIRef]] = get_d2g_exclusions_by_curator()
-    digenic_protection_gene_pheno_orcids: Dict[Tuple[str, str, str], Optional[URIRef]] = get_d2g_digenic_protections()
+    protections_gene_pheno__hgnc_orcid_map: Dict[Tuple[str, str], Tuple[str, Optional[URIRef]]] = \
+        get_d2g_digenic_protections()
     for p_mim, assocs in phenotype_genes.items():
         for assoc in assocs:
             gene_mim, p_lab, p_map_key, p_map_lab = assoc['gene_id'], assoc['phenotype_label'], \
@@ -383,17 +384,12 @@ def omim2obo(use_cache: bool = False):
             evidence = f'Evidence: ({p_map_key}) {p_map_lab}'
             p_mim_excluded = p_mim in exclusions_p_mim_orcid_map
             protected_digenic_key = (p_mim, gene_mim)
-            
-            # reduce key tuple back to only phenotype and gene mim identifiers to check for protected_digenic_key in protected-disease-gene.tsv 
-            reduced_map_digenic_protection_gene_pheno_orcids = {
-                (p_mim_protected, gene_mim_protected): (hgnc_id_protected, orcid_protected) for (p_mim_protected, gene_mim_protected, hgnc_id_protected), orcid_protected in digenic_protection_gene_pheno_orcids.items()
-            }
-            protected_digenic_assoc: bool = protected_digenic_key in reduced_map_digenic_protection_gene_pheno_orcids
 
+            protected_digenic_assoc: bool = protected_digenic_key in protections_gene_pheno__hgnc_orcid_map
             if protected_digenic_assoc:
                 hgnc_id_protected: str
                 orcid_protected: Optional[URIRef]
-                hgnc_id_protected, orcid_protected = reduced_map_digenic_protection_gene_pheno_orcids[protected_digenic_key]
+                hgnc_id_protected, orcid_protected = protections_gene_pheno__hgnc_orcid_map[protected_digenic_key]
                 add_gene_disease_associations(graph, gene_mim, p_mim, evidence, orcid_protected)
                 graph.add((OMIM[gene_mim], SKOS.exactMatch, HGNC[hgnc_id_protected]))
                 continue
